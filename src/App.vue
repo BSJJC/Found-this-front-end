363ef62cbd532d72ee224df418c16d0e2f3ff8ec
BSJJC <805086021@qq.com>
2023-03-19 13:12:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-view v-slot="{ Component }">
+    <Component :is="Component" />
+  </router-view>
+</template>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-red-500 h-20 w-20">Home page</div>
+  <el-button>test btn</el-button>
+</template>
+
+<style scoped></style>
