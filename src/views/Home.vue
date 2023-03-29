1df37fa1bd0450847156c5a7249cbd458a333e77
BSJJC <805086021@qq.com>
2023-03-29 11:34:40 +0800

    chore

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,18 @@ const NavBar = defineAsyncComponent(
 const MainPage = defineAsyncComponent(
   () => import("@/components/MainPage/MainPage.vue")
 );
+const SquareBackground = defineAsyncComponent(
+  () => import("@/components/Effects/SquareBackground.vue")
+);
 </script>
 
 <template>
-  <div class="h-screen overflow-hidden">
-    <transition>
-      <nav-bar></nav-bar>
-    </transition>
+  <square-background
+    class="w-screen h-screen absolute top-0 z-0"
+  ></square-background>
+
+  <div class="h-screen w-screen overflow-hidden relative">
+    <nav-bar class="z-[50]"></nav-bar>
 
     <el-scrollbar height="90%">
       <main-page></main-page>
