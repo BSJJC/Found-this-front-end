aecb7388d2943625b74ef93d14eed0794418b057
BSJJC <805086021@qq.com>
2023-04-24 10:02:47 +0800

    chore

--- src/views/TopicDetail.vue
+++ src/views/TopicDetail.vue
@@ -1,7 +1,16 @@
 <script lang="ts" setup>
-import { ref, reactive, onBeforeMount, onMounted } from "vue";
+import {
+  ref,
+  reactive,
+  onBeforeMount,
+  onMounted,
+  defineAsyncComponent,
+} from "vue";
 
 import Topic from "@/components/MainPage/Topic.vue";
+const NavBar = defineAsyncComponent(
+  () => import("@/components/Navbar/NavBar.vue")
+);
 
 const showMask = ref(true);
 
@@ -44,7 +53,24 @@ onMounted(() => {
     class="fixed w-screen h-screen bg-[#7E56DA]"
   ></topic>
 
-  <div class="w-16 h-16 bg-red-300"></div>
+  <!-- main -->
+
+  <main class="flex justify-center items-center w-screen h-screen p-3">
+    <!-- topic details -->
+    <div class="h-full w-[40%] bg-red-300 flex justify-start items-center">
+      <div
+        class="w-[95%] h-[95%] bg-yellow-200 rounded-3xl flex justify-center items-center flex-col p-12"
+      >
+        <!-- title -->
+        <div class="w-full bg-red-200 text-3xl">
+          {{ topicDeatil.title }}
+        </div>
+      </div>
+    </div>
+
+    <!-- replies -->
+    <div class="flex h-full w-[60%] bg-blue-300">2</div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
