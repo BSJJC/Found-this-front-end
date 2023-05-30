629e2131da42423ada8cc333c9ebd1a76b3396b6
BSJJC <805086021@qq.com>
2023-05-30 13:10:22 +0800

    chore

--- src/components/MainPage/Topic.vue
+++ src/components/MainPage/Topic.vue
@@ -23,23 +23,15 @@ const props = defineProps<topicInfoType>();
 <template>
   <div class="topic flex justify-center items-center w-1/3 h-[400px] mb-5">
     <div
-      class="w-[90%] max-w-[800px] h-[100%] max-h-[600px] rounded-xl overflow-hidden p-[30px] bg-white shadow-lg shadow-[#d5d5d5] transition duration-300 hover:cursor-pointer hover:shadow-xl hover:shadow-[#7e56da] hover:scale-105 active:scale-100"
-    >
+      class="w-[90%] max-w-[800px] h-[100%] max-h-[600px] rounded-xl overflow-hidden p-[30px] bg-white shadow-lg shadow-[#d5d5d5] transition duration-300 hover:cursor-pointer hover:shadow-xl hover:shadow-[#7e56da] hover:scale-105 active:scale-100">
       <!-- title -->
-      <div
-        class="w-full h-1/3 rounded-xl overflow-hidden flex justify-start items-center relative"
-      >
-        <div
-          class="absolute w-full h-full flex justify-start items-center p-8 z-10 text-white text-3xl"
-        >
+      <div class="w-full h-1/3 rounded-xl overflow-hidden flex justify-start items-center relative">
+        <div class="absolute w-full h-full flex justify-start items-center p-8 z-10 text-white text-3xl">
           {{ props.topicInfo.title.slice(0, 20) }}
         </div>
 
-        <background-img
-          :src="`http://localhost:5000/api/topic/bg/download/${props.topicInfo.bgID}`"
-          alt="test"
-          class="absolute z-0"
-        ></background-img>
+        <background-img :src="`http://localhost:5000/api/topic/bg/download/${props.topicInfo.bgID}`" alt="test"
+          class="absolute z-0"></background-img>
       </div>
 
       <!-- info -->
@@ -47,9 +39,7 @@ const props = defineProps<topicInfoType>();
         <div class="w-full h-full flex justify-center items-center flex-row">
           <!-- topics -->
           <div class="w-[25%] h-full flex justify-center items-center flex-col">
-            <div
-              class="w-full h-1/3 text-md flex justify-start items-center text-[#adadad]"
-            >
+            <div class="w-full h-1/3 text-md flex justify-start items-center text-[#adadad]">
               REPLIES
             </div>
             <div class="w-full h-2/3 text-2xl text-[#5d5d5d]">114514</div>
@@ -58,9 +48,7 @@ const props = defineProps<topicInfoType>();
           <!-- last active -->
           <div class="w-[75%] h-full">
             <div class="w-full h-full flex justify-center items-center flex-col pl-8">
-              <div
-                class="w-full h-1/3 text-md flex justify-start items-center flex-row text-[#adadad]"
-              >
+              <div class="w-full h-1/3 text-md flex justify-start items-center flex-row text-[#adadad]">
                 LAST ACTIVE
               </div>
               <div class="w-full h-2/3 text-2xl text-[#5d5d5d]">114514</div>
@@ -70,20 +58,14 @@ const props = defineProps<topicInfoType>();
 
         <!-- last topic -->
         <div class="w-full h-full flex justify-center items-center flex-col">
-          <div
-            class="w-full h-1/3 text-md flex justify-start items-center flex-row text-[#adadad]"
-          >
+          <div class="w-full h-1/3 text-md flex justify-start items-center flex-row text-[#adadad]">
             LAST REPLY
           </div>
-          <div
-            class="w-full h-2/3 text-2xl text-[#5d5d5d] flex justify-center items-center flex-row"
-          >
+          
+          <div class="w-full h-2/3 text-2xl text-[#5d5d5d] flex justify-center items-center flex-row">
             <div class="h-full w-[20%] flex justify-start items-center">
-              <Avater
-                :src="`http://localhost:5000/api/userAvaters/get/${props.topicInfo.founderAvaterID}`"
-                alt="topic founder avatar"
-                loading-text="loading"
-              ></Avater>
+              <Avater :src="`http://localhost:5000/api/userAvaters/get/${props.topicInfo.founderAvaterID}`"
+                alt="topic founder avatar" loading-text="loading"></Avater>
             </div>
             <div class="h-full w-[80%] text-[1.2rem] overflow-clip">
               Lorem ipsum, dolor sit amet consectetur consectetur consectetur consectetur
